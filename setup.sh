17c8aa5574819b8ce1b52e8d94d4ffbf08a2eae1
Tomasz Klim <github@tomaszklim.pl>
2018-10-12 22:42:39 +0200

    create skip-smart.raid file

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ if [ ! -f /etc/local/.config/skip-smart.devices ]; then
 # /dev/disk/by-id/ata-ST4000DM000-1F2168_W300XXXX
 " >/etc/local/.config/skip-smart.devices
 fi
+
+if [ ! -f /etc/local/.config/skip-smart.raid ]; then
+	echo "# devices to skip should be added in the below format:
+# sas-ST3600057SS_6SLXXXXX
+" >/etc/local/.config/skip-smart.raid
+fi
